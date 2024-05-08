637040ab352c201faf267872bc770e79f2bbf3bc
malo <ff@fake.com>
2024-05-08 10:50:28 +0530

    a login form and react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 //import Game from './TicTacToe.tsx'
-import App  from './App.tsx';
-import './index.css'
+//import App  from './App.tsx';
+import './reactrouter.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
