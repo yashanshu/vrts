1ccaf48b825fca4366b6bc55aa1384379d8a7e7c
malo <ff@fake.com>
2024-05-09 10:57:22 +0530

    working with router.

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 //import Game from './TicTacToe.tsx'
 //import App  from './App.tsx';
-import './reactrouter.css'
+import "./reactrouter.css";
+import Root, {loader as rootLoader} from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
